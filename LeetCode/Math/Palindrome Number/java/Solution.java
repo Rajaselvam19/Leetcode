915c42566380d10ea95a77e3c69a30ddf0ba0634
Rajaselvam19 <rajaselvamofficial@gmail.com>
2026-07-31 11:56:56 +0530

    feat: Add Palindrome Number

--- LeetCode/Math/Palindrome Number/java/Solution.java
+++ LeetCode/Math/Palindrome Number/java/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
         int num=x;
         int temp=0;
-        while(x!=0){
-            int a=x%10;
-            temp=temp*10+a;
-          x=x/10;
-        } 
-        return num==temp;
+        while(x>0){
+            int arr=x%10;
+            temp=10*temp+arr;
+            x=x/10;
+        }
+        if(temp==num){
+            return true;
+        }
+         
+         return false;
     }
 }
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This repository contains my personal library of highly optimized, strictly teste
 ## 📊 Real-Time Performance Analytics
 
 <p align="center">
-  <img src="https://github-readme-leetcode-card.romitsagu.com/Rajaofficial_19?theme=tokyonight&show=graph,recent&v=1785229078150" width="100%" />
+  <img src="https://github-readme-leetcode-card.romitsagu.com/Rajaofficial_19?theme=tokyonight&show=graph,recent&v=1785479213558" width="100%" />
 </p>
 
 ## 📂 Repository Structure
